906bbb432ece8d390e4d8908a3f04ca2c74cc86d
Kate Kirychenka <kate.kirychenka@itrexgroup.lan>
2021-09-14 14:33:54 +0300

    promise all

--- app.js
+++ app.js
@@ -2,64 +2,47 @@ const fs = require("fs");
 const path = require("path");
 const zlib = require("zlib");
 
-
 const inpPath = "./input";
 const outPath = "./output";
 
-
-const folderPromise = new Promise((res, rej)=>{
-
-  fs.readdir(inpPath,(err,folder)=>{
+const folderPromise = new Promise((res, rej) => {
+  fs.readdir(inpPath, (err, folder) => {
     if (err) {
       console.log(err);
     } else {
       const fileNames = folder;
-      res(fileNames)
-      console.log(fileNames)
-  }
-})})
+      res(fileNames);
+      console.log(fileNames);
+    }
+  });
+});
 
-folderPromise.then((data)=>{
-  const promises = []
-  data.forEach((i)=>{
-    
+folderPromise.then((data) => {
+  const promises = [];
+
+  data.forEach((i) => {
     promises.push(
-      new Promise((res,rej)=>{
-
+      new Promise((res, rej) => {
         const readStream = fs.createReadStream(path.join(inpPath, i));
-      
-   
+        console.log("Create read stream");
+
         const writeStream = fs.createWriteStream(path.join(outPath, i));
-        
-   
+        console.log("Create write stream");
+
         const compressStream = zlib.createGzip();
+        console.log("Create zip");
 
-
-        readStream.on('data', () => {
-          
-          console.log('Have data');
-                 
-               }).pipe(compressStream).pipe(writeStream)
-               
-      writeStream.on('end', res )
-              })
-              
-      
-    )})
-    
-    return Promise.all(promises).then((data)=>{
-      console.log(`${data}`)
-    })
-    
-})
-
-
-
-
-
-
-
-
-
-    
-
+        readStream
+          .on("data", () => {
+            console.log("Have data");
+          })
+          .pipe(compressStream)
+          .pipe(writeStream)
+          .on("end", () => res);
+      })
+    );
+  });
+  return Promise.all([promises]).then(() => {
+    console.log("Copy all files done");
+  });
+});
