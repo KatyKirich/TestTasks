99b830b1e7ac365f7eedb4558ebea6eafd449ad0
Kate <katy8kirichenko@gmail.com>
2021-09-22 16:29:19 +0300

    module done

--- movingAverageUSD/index.js
+++ movingAverageUSD/index.js
@@ -1,36 +1,11 @@
-const https = require("https");
-const getURL = require("./module/getURL");
-const createTotalArr = require("./module/crateTotalArr");
+const getRates = require("./module/index");
 
 const usdIdAfter0708 = 431;
 const startDate = new Date(2020, 11, 1).toUTCString();
 const today = new Date().toUTCString();
+const date = "Date";
+const curOffRate = "Cur_OfficialRate";
 
-const getRates = (id) =>
-  new Promise((res, rej) => {
-    let dataRates = "";
-
-    https
-      .get(getURL(id, startDate, today), (httpsResponse) => {
-        httpsResponse.on("data", (data) => {
-          dataRates += data.toString();
-        });
-
-        httpsResponse.on("error", rej);
-
-        httpsResponse.on("end", () => res(dataRates));
-      })
-      .on("error", rej);
-  })
-    .then(JSON.parse)
-    .then((res) =>
-      res.map((el) => {
-        delete el.Cur_ID;
-        return el;
-      })
-    );
-
-getRates(usdIdAfter0708).then((data) => {
-  const result = createTotalArr(data);
-  console.log(result);
-});
+getRates(usdIdAfter0708, startDate, today, date, curOffRate).then(() =>
+  console.log("Job is done!")
+);
