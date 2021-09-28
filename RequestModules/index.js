26b1fde295514946e36d320a7ac60c8e2b60fb4b
Kate Kirychenka <kate.kirychenka@itrexgroup.lan>
2021-09-28 18:00:17 +0300

    start working with post

--- RequestModules/index.js
+++ RequestModules/index.js
@@ -1,7 +1,27 @@
 const requestGet = require("./module/RequestGet");
 const MyURL = new URL("https://www.nbrb.by/api/exrates/rates?periodicity=0");
 
+const requestPost = require("./module/RequestPost");
+
 requestGet(MyURL).then((result) => {
   console.log(result);
   console.log("Request GET is done!");
 });
+
+// do POST
+const options = {
+  hostname: "www.google.com",
+  port: 80,
+  path: "/upload",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Content-Length": Buffer.byteLength(postData),
+  },
+};
+
+const postData = JSON.stringify({
+  msg: "Hello World!",
+});
+
+requestPost(options, postData);
---
+++ RequestModules/module/RequestPost.js
@@ -0,0 +1,25 @@
+const http = require("http");
+
+function requestPost(options, myData) {
+  const req = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding("utf8");
+    res.on("data", (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+    res.on("end", () => {
+      console.log("No more data in response.");
+    });
+  });
+
+  req.on("error", (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+  // Write data to request body
+  req.write(myData);
+  req.end();
+}
+
+module.exports = requestPost;
